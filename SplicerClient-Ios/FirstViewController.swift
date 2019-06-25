40066378471b522f27d7a95f6a99ac4d74c800c5
Aaron Evans <aaron@splicer.io>
2019-06-25 12:47:36 -0400

    generated new tabbed app from xcode 8.2.1

---
+++ SplicerClient-Ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SplicerClient-Ios
+//
+//  Created by Aaron Evans on 6/25/19.
+//  Copyright © 2019 Aaron Evans. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
