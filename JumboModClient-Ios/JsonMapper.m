3cd479e5bbc0001362a7a74961fe106c7e272cb6
Aaron Evans <aaron@splicer.io>
2019-07-19 20:10:49 -0700

    fully generated: displays zip data

--- JumboModClient-Ios/JsonMapper.m
+++ JumboModClient-Ios/JsonMapper.m
@@ -1,12 +1,4 @@
-//  Copyright (c) 2014 Jumbo Mobile. All rights reserved.
-//
-
 #import "JsonMapper.h"
-/*
-#import "User.h"
-#import "SplicerRoot.h"
-#import "Address.h"
- */
 #import "SplicerClient-Swift-Bridging-Header.h"
 
 @implementation JsonMapper
@@ -14,51 +6,24 @@
 - (RKObjectMapping *)mapComplexTypes
 {
     //generated
-    RKObjectMapping *ModelRootMapping = [RKObjectMapping mappingForClass:[ModelsSplicerSplicerRoot class]];
-    [ModelRootMapping addAttributeMappingsFromDictionary:@{
-                                                           @"id": @"id__"
-                                                           }];
-     //mapKeyPath:@"id" toAttribute:@"Id_"];
-  
+    RKObjectMapping *SplicerRootMapping = [RKObjectMapping mappingForClass:[ModelsSplicerSplicerRoot class]];
+    [SplicerRootMapping addAttributeMappingsFromDictionary:@{@"id":@"id__" }];
     RKObjectMapping *UserMapping = [RKObjectMapping mappingForClass:[ModelsSplicerUser class]];
-    [UserMapping addAttributeMappingsFromDictionary:@{
-                                                      @"id": @"id__",
-                                                      @"lastname": @"lastname_"
-                                                      }];
-     //mapKeyPath:@"id" toAttribute:@"Id_"];
-      /*
-    [UserMapping mapKeyPath:@"firstName" toAttribute:@"firstName"];
-    [UserMapping mapKeyPath:@"lastName" toAttribute:@"lastName"];
-       */
-    
-    
+    [UserMapping addAttributeMappingsFromDictionary:@{@"firstname":@"firstname_" }];
+    [UserMapping addAttributeMappingsFromDictionary:@{@"lastname":@"lastname_" }];
+    [UserMapping addAttributeMappingsFromDictionary:@{@"id":@"id__" }];
     RKObjectMapping *AddressMapping = [RKObjectMapping mappingForClass:[ModelsSplicerAddress class]];
-    [AddressMapping addAttributeMappingsFromDictionary:@{
-                                                      @"id": @"id__",
-                                                      @"zip": @"zip_"
-                                                      }];
-     
-
-    /*
-    [AddressMapping mapKeyPath:@"id" toAttribute:@"Id_"];
-    [AddressMapping mapKeyPath:@"state" toAttribute:@"state"];
-    [AddressMapping mapKeyPath:@"street1" toAttribute:@"street1"];
-    [AddressMapping mapKeyPath:@"street2" toAttribute:@"street2"];
-    [AddressMapping mapKeyPath:@"city" toAttribute:@"city"];
-    [AddressMapping mapKeyPath:@"zip" toAttribute:@"zip"];
-       */
-    [ModelRootMapping addRelationshipMappingWithSourceKeyPath:@"user" mapping:UserMapping];
-    
-    
-   // [UserMapping addRelationshipMappingWithSourceKeyPath:@"address" mapping:AddressMapping];
-    
+    [AddressMapping addAttributeMappingsFromDictionary:@{@"city":@"city_" }];
+    [AddressMapping addAttributeMappingsFromDictionary:@{@"state":@"state_" }];
+    [AddressMapping addAttributeMappingsFromDictionary:@{@"street1":@"street1_" }];
+    [AddressMapping addAttributeMappingsFromDictionary:@{@"street2":@"street2_" }];
+    [AddressMapping addAttributeMappingsFromDictionary:@{@"zip":@"zip_" }];
+    [AddressMapping addAttributeMappingsFromDictionary:@{@"id":@"id__" }];
+    [SplicerRootMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"user" toKeyPath:@"user_" withMapping:UserMapping]];
     [UserMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"address" toKeyPath:@"address_" withMapping:AddressMapping]];
-    
-    
-    //mapKeyPath:@"user" toRelationship:@"user" withMapping:UserMapping];
-    //[UserMapping mapKeyPath:@"address" toRelationship:@"address" withMapping:AddressMapping];
 
     //end generated
+
     return UserMapping;
 }
 
