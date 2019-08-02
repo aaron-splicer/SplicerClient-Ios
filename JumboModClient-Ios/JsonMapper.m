8740dfc5dbf2cd513b0a6ec742d2f3694cd73abf
Aaron Evans <aaron@splicer.io>
2019-08-01 17:57:30 -0700

    manually modified: displays all forms/data again

--- JumboModClient-Ios/JsonMapper.m
+++ JumboModClient-Ios/JsonMapper.m
@@ -9,8 +9,8 @@
     RKObjectMapping *SplicerRootMapping = [RKObjectMapping mappingForClass:[ModelsSplicerSplicerRoot class]];
     [SplicerRootMapping addAttributeMappingsFromDictionary:@{@"id":@"id__" }];
     RKObjectMapping *PersonMapping = [RKObjectMapping mappingForClass:[ModelsSplicerPerson class]];
-    [PersonMapping addAttributeMappingsFromDictionary:@{@"firstname":@"firstname_" }];
-    [PersonMapping addAttributeMappingsFromDictionary:@{@"lastname":@"lastname_" }];
+    [PersonMapping addAttributeMappingsFromDictionary:@{@"firstName":@"firstName_" }];
+    [PersonMapping addAttributeMappingsFromDictionary:@{@"lastName":@"lastName_" }];
     [PersonMapping addAttributeMappingsFromDictionary:@{@"id":@"id__" }];
     RKObjectMapping *AddressMapping = [RKObjectMapping mappingForClass:[ModelsSplicerAddress class]];
     [AddressMapping addAttributeMappingsFromDictionary:@{@"city":@"city_" }];
@@ -19,8 +19,8 @@
     [AddressMapping addAttributeMappingsFromDictionary:@{@"street2":@"street2_" }];
     [AddressMapping addAttributeMappingsFromDictionary:@{@"zip":@"zip_" }];
     [AddressMapping addAttributeMappingsFromDictionary:@{@"id":@"id__" }];
-    [SplicerRootMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"Person" toKeyPath:@"Person_" withMapping:PersonMapping]];
-    [PersonMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"Address" toKeyPath:@"Address_" withMapping:AddressMapping]];
+    [SplicerRootMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"person" toKeyPath:@"person_" withMapping:PersonMapping]];
+    [PersonMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"address" toKeyPath:@"address_" withMapping:AddressMapping]];
 
     //end generated
 
